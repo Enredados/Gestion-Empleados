345dd2a34a13d39999ed92903ff761312e35da38
xlcaceres <lizxime4ever@hotmail.com>
2021-05-10 09:47:45 -0500

    proyectoEmpleado

---
+++ proyectoEmpleados/proyectoEmpleados/src/proyectoempleados/empleado.java
@@ -0,0 +1,38 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectoempleados;
+
+/**
+ *
+ * @author alang
+ */
+public class empleado {
+    private String nombre;
+    private String codigo;
+    private int sueldo;
+    
+    public empleado(String nombre, int sueldo){
+        this.nombre = nombre;
+        this.sueldo = sueldo;
+    }
+    
+    public String obtenerNombre(){
+        return nombre;
+    }
+    
+    public String obtenerCodigo(){
+        return codigo;
+    }
+    
+    public int obtenerSueldo(){
+        return sueldo;
+    }
+    
+    public void setearCodigo(String codigo){
+        this.codigo = codigo;
+    }
+    
+}
